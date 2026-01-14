51915708aed6fe981fbced4e3c77c16357330b20
iudah <olaolujude@gmail.com>
2026-01-14 07:17:22 +0100

    Remove redundant flag

--- common.cmake
+++ common.cmake
@@ -1,6 +1,4 @@
 set(C_WARNINGS "-Wall -Wextra -Wshadow -Wformat=2 -Wunused")
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DDEBUG -ggdb -O0 -fno-omit-frame-pointer -fsanitize=address")
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -g")
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_BINARY_DIR}/memalloc/lib/cmake/memalloc"
-    "${CMAKE_BINARY_DIR}/zot/lib/cmake/zot" "${CMAKE_BINARY_DIR}/grey/lib/cmake/grey")
-
+     "${CMAKE_BINARY_DIR}/zot/lib/cmake/zot"
+     "${CMAKE_BINARY_DIR}/grey/lib/cmake/grey")
